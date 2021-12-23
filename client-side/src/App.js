258224ca75a441286575e9291cc7e963f1b07e2a
raihanr1 <raihanrobbani123@gmail.com>
2021-12-24 02:44:15 +0700

    add pure styling and layouting with css

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route
+        path="*"
+        element={
+          <center style={{ marginTop: "100px" }}>
+            <Link
+              to="/"
+              style={{
+                textDecoration: "none",
+                color: "black",
+              }}
+            >
+              <img src="https://i.gifer.com/XOsX.gif" alt="duckWalk" />
+              Oops page not found, go to dashboard Click here.
+            </Link>
+          </center>
+        }
+      />
+    </Routes>
   );
 }
 
