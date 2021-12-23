9c89dff044fb72fe0f61be4bce4dd48d3d533b75
raihanr1 <raihanrobbani123@gmail.com>
2021-12-24 02:49:52 +0700

    add transaction type store from react-redux

---
+++ client-side/src/store/action-type/transactionType.js
@@ -0,0 +1,5 @@
+export const GET_USER_INFORMATION_TRANSACTION =
+  "get/user_information_transaction";
+
+export const LOADING_SERVER = "loading/server";
+export const ERROR_SERVER = "error/server";
