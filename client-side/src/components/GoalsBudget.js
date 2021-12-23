1a0a5d9d72694bac0ed6c20292641f85b256078f
raihanr1 <raihanrobbani123@gmail.com>
2021-12-24 02:47:13 +0700

    add component goals budget for new feature

---
+++ client-side/src/components/GoalsBudget.js
@@ -0,0 +1,8 @@
+export default function GoalsBudget() {
+  return (
+    <div style={{ marginLeft: "20px" }}>
+      <h1 className="sub">Goals Budget</h1>
+      <div className="goals-budget-container"></div>
+    </div>
+  );
+}
