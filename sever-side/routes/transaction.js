369e1547551dfaeb0b7dedbca6bbb1f2bfdd4847
raihanr1 <raihanrobbani123@gmail.com>
2021-12-23 16:28:11 +0700

    endpoint for transaction users

--- sever-side/routes/transaction.js
+++ sever-side/routes/transaction.js
@@ -1,3 +1,19 @@
 const router = require('express').Router()
-router.get('/')
+const authentication = require('../middleware/authentication')
+const {
+    responseGetTransaction,
+    responseTopUpBalance,
+    responseTransferUser
+} = require('../controllers/transactions/index')
+const errorHandle = require('../middleware/errorHandle')
+
+router.get('/', authentication, responseGetTransaction)
+
+router.patch('/topup', authentication,responseTopUpBalance)
+router.patch('/transfer',authentication, responseTransferUser)
+router.patch('/payment',authentication)
+router.patch('/',authentication)
+
+router.use(errorHandle)
+
 module.exports = router
