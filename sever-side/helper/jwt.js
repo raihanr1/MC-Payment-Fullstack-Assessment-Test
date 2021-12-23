e0ee103796012967379aa2f9c20282f944442be0
raihanr1 <raihanrobbani123@gmail.com>
2021-12-23 16:26:43 +0700

    create json web token

--- sever-side/helper/jwt.js
+++ sever-side/helper/jwt.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const signToken = (payload) => {
-    console.log(process.env.PRIVATE_KEY_JWT, 'ga ada?')
     return jwt.sign(payload, process.env.PRIVATE_KEY_JWT)
 }
 
