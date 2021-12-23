94d0690246c2560c563ad6ce6532adde028d75d2
raihanr1 <raihanrobbani123@gmail.com>
2021-12-23 16:22:45 +0700

    controller for balance manager features

--- sever-side/controllers/transactions/balance.js
+++ sever-side/controllers/transactions/balance.js
@@ -1,8 +1,31 @@
-const {Balance} = require('../../models')
-
+const {Balance, sequelize} = require('../../models')
 const createBalance = async (input) => {
     let response = await Balance.create(input)
     return response
 }
 
-module.exports = createBalance
+const updateBalance = async (account_id, amount,t) => {
+    let response = await Balance.update(amount, {
+        where: {
+            account_id
+        },
+        transaction: t,
+        returning: true,
+    })
+    return response
+}
+
+const findBalanceUser = async (account_id) => {
+    let response = await Balance.findOne({
+        where: {
+            account_id
+        }
+    })
+    return response
+}
+
+module.exports = {
+    createBalance,
+    updateBalance,
+    findBalanceUser
+}
