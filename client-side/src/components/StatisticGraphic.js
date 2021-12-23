f6049a5d61c2b845bc5ede801074cab3a73186dd
raihanr1 <raihanrobbani123@gmail.com>
2021-12-24 02:48:12 +0700

    add statistic graphic component transaction

---
+++ client-side/src/components/StatisticGraphic.js
@@ -0,0 +1,8 @@
+export default function StatisticGraphic() {
+  return (
+    <div style={{ marginLeft: "20px" }}>
+      <h1 className="sub">Statistic Graphic</h1>
+      <div className="statistic-container"></div>
+    </div>
+  );
+}
