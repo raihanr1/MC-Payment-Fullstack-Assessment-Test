60d66663da1a1a6d65f80b22e6e4d83b999e9121
raihanr1 <raihanrobbani123@gmail.com>
2021-12-26 14:52:11 +0700

    add route path login

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/Home";
+import LoginPage from "./pages/login";
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<LoginPage />}></Route>
       <Route path="/" element={<HomePage />} />
       <Route
         path="*"
