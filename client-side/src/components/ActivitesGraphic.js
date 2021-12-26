011e8d5d0e0e59def74c6868d525532fbb2ca2b0
raihanr1 <raihanrobbani123@gmail.com>
2021-12-26 14:53:07 +0700

    adding style

--- client-side/src/components/ActivitesGraphic.js
+++ client-side/src/components/ActivitesGraphic.js
@@ -2,6 +2,7 @@ export default function ActivitiesGraphic() {
   return (
     <div className="activities-container">
       <h1>Activities</h1>
+      <canvas id="speedChart" width="600" height="400"></canvas>
     </div>
   );
 }
