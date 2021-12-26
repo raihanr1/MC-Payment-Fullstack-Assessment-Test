586c557b60aba081ed86172c3c0796db57bc0cda
raihanr1 <raihanrobbani123@gmail.com>
2021-12-26 17:50:27 +0700

    add authentication middleware

--- sever-side/middleware/authentication.js
+++ sever-side/middleware/authentication.js
@@ -1,26 +1,26 @@
-const {User} = require('../models')
-const {getPayload} = require('../helper/jwt')
+const { User } = require("../models");
+const { getPayload } = require("../helper/jwt");
 const authentication = async (req, res, next) => {
-    try {
-        if (!req.headers.access_token) {
-            throw {
-                message: 'Please login first'
-            }
-        }
-        let {id, email} = getPayload(req.headers.access_token)
-        let response = await User.findOne({
-            where: {id, email}
-        })
-        if (!response) {
-            throw {
-                message: 'User not found'
-            }
-        }
-        req.user = response
-        next()
-    } catch (error) {
-        next(error)
+  try {
+    if (!req.headers.access_token) {
+      throw {
+        message: "Please login first",
+      };
     }
-}
+    let { id, email } = getPayload(req.headers.access_token);
+    let response = await User.findOne({
+      where: { id, email },
+    });
+    if (!response) {
+      throw {
+        message: "User not found",
+      };
+    }
+    req.user = response;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = authentication
+module.exports = authentication;
