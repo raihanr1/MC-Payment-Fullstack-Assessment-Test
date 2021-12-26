d47dd9f8c035e74939725cadc90432168bad7ad7
raihanr1 <raihanrobbani123@gmail.com>
2021-12-26 14:56:40 +0700

    add case on reducer for action user

--- client-side/src/store/reducer/transactionReducer.js
+++ client-side/src/store/reducer/transactionReducer.js
@@ -1,7 +1,10 @@
 import {
   GET_USER_INFORMATION_TRANSACTION,
   LOADING_SERVER,
+  TRANSFER_TRANSACTION,
+  TOPUP_TRANSACTION,
   ERROR_SERVER,
+  USER_LOGOUT,
 } from "../action-type/transactionType";
 
 let initialState = {
@@ -17,6 +20,28 @@ export default function transactionReducer(state = initialState, action) {
         ...state,
         usersInformation: action.payload,
       };
+    case TOPUP_TRANSACTION:
+      let topUp = state.usersInformation;
+      topUp.balance[0].amount = action.payload.amount;
+      topUp.transaction.push(action.payload.transaction);
+      topUp.transaction.sort((a, b) => {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      });
+      return {
+        ...state,
+        usersInformation: topUp,
+      };
+    case TRANSFER_TRANSACTION:
+      let transfer = state.usersInformation;
+      transfer.balance[0].amount = action.payload.amount;
+      transfer.transaction.push(action.payload.transaction);
+      transfer.transaction.sort((a, b) => {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      });
+      return {
+        ...state,
+        usersInformation: transfer,
+      };
     case LOADING_SERVER:
       return {
         ...state,
@@ -26,6 +51,12 @@ export default function transactionReducer(state = initialState, action) {
       return {
         ...state,
         error: action.payload,
+      };
+    case USER_LOGOUT:
+      return {
+        usersInformation: {},
+        loading: false,
+        error: null,
       };
     default:
       return state;
