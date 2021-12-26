6c4da624bcb9f1fff4f3305ed9acf14c6ae5e2af
raihanr1 <raihanrobbani123@gmail.com>
2021-12-26 14:55:23 +0700

    add transaction history container

--- client-side/src/components/TransactionHistory.js
+++ client-side/src/components/TransactionHistory.js
@@ -1,16 +1,37 @@
 import CardTransaction from "./CardTransaction";
 import { useSelector } from "react-redux";
-
+const styles = {
+  fontSize: "10px",
+  marginLeft: "10px",
+  marginTop: "35px",
+};
 export default function TransactionHistory() {
   const { usersInformation, loading, error } = useSelector(
     (state) => state.transaction
   );
   return (
     <div>
-      <h1>Transaction History</h1>
+      <div style={{ display: "flex" }}>
+        <h1>Transaction History</h1>
+        <p style={styles}>
+          <span className="dot" style={{ backgroundColor: "#88E0EF" }}></span>
+          Top Up
+        </p>
+        <p style={styles}>
+          <span className="dot" style={{ backgroundColor: "#FABB51" }}></span>
+          Transfer
+        </p>
+        <p style={styles}>
+          <span
+            className="dot"
+            style={{ backgroundColor: "rgb(52, 88, 196)" }}
+          ></span>
+          Payment
+        </p>
+      </div>
       <div className="transaction-container">
-        <div className="scroolbar-content">
-          {usersInformation?.transaction
+        <div className="scroolbar-content" style={{ cursor: "all-scroll" }}>
+          {usersInformation.transaction && !loading
             ? usersInformation.transaction.map((el, i) => {
                 return <CardTransaction transaction={el} key={i} />;
               })
